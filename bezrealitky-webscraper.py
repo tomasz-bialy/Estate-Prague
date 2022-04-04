04eb04911f67a779a3673ea5c35d32a4d4574d8e
Tomasz Bialy <t1.bialy@gmail.com>
2022-04-04 19:54:29 +0200

    Updated

--- bezrealitky-webscraper.py
+++ bezrealitky-webscraper.py
@@ -14,27 +14,44 @@ try:
     GITHUB_API_KEY = os.environ['GITHUB_API_KEY']
     print('Loaded API key from .env file.')
 except:
-    print('Could not load API key from the .env file.')
+    print('Could not load the .env file. Loading API key from environmental variable.')
+    GITHUB_API_KEY = os.environ['GITHUB_API_KEY']
 
 df = pd.DataFrame(columns=['Listing ID','Price','Latitude','Longitude','District'])
 
 current_page = 1
 max_pages = 999
+wait_time = 0
 
 while current_page <= max_pages:
     while True:
         try:  
-            time.sleep(random.randrange(1,2))
+            time.sleep(random.randrange(1,4))
             url = f'https://www.bezrealitky.com/listings/offer-rent/flat/praha?&page={current_page}'
-            headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+            headers =   {
+                        'Connection': 'keep-alive',
+                        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+                        'sec-ch-ua-mobile': '?0',
+                        'sec-ch-ua-platform': '"Windows"',
+                        'Upgrade-Insecure-Requests': '1',
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                        'Sec-Fetch-Site': 'none',
+                        'Sec-Fetch-Mode': 'navigate',
+                        'Sec-Fetch-User': '?1',
+                        'Sec-Fetch-Dest': 'document',
+                        'Accept-Language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5,de;q=0.4,cs;q=0.3,ja;q=0.2'
+                        }
             response = requests.get(url,headers=headers)
             soup = bs4.BeautifulSoup(response.text,"lxml")
 
             max_pages = int(soup.find_all("a",class_='page-link pagination__page')[-2].text)
             listings_in_page = soup.find_all(class_='product__link')
+            print(f'Parsing page {current_page} of {max_pages}')
         except:
-            print('Error during parsing the data. Waiting 3 minutes and will try again')
-            time.sleep(180)
+            wait_time = wait_time+300
+            print(f'Error during parsing the data. Waiting {wait_time/60} minutes and will try again')
+            time.sleep(wait_time)
             continue
         break
 
@@ -43,7 +60,7 @@ while current_page <= max_pages:
     for apartment in listings_in_page:
         while True:
             try:
-                time.sleep(random.randrange(1,2))
+                time.sleep(random.randrange(1,4))
                 apartment_url = apartment.get('href')
                 url2 = 'https://www.bezrealitky.com' + apartment_url
                 response2 = requests.get(url2,headers=headers)
@@ -72,8 +89,9 @@ while current_page <= max_pages:
                 df_single_listing = pd.DataFrame([dictionary_data])
                 df = pd.concat([df,df_single_listing],axis=0,ignore_index=True)
             except:
-                print('Error during parsing the data. Waiting 3 minutes and will try again')
-                time.sleep(180)
+                wait_time = wait_time+300
+                print(f'Error during parsing the data. Waiting {wait_time/60} minutes and will try again')
+                time.sleep(wait_time)
                 continue
             break
     current_page += 1 

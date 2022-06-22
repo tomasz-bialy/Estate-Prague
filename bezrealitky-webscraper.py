ba839e6d87e4b3b86e361405ae92ae3afc9d5ae0
Tomasz Bialy <t1.bialy@gmail.com>
2022-06-23 01:03:22 +0200

    Updated for Heroku

--- bezrealitky-webscraper.py
+++ bezrealitky-webscraper.py
@@ -3,6 +3,7 @@ import bs4
 import time
 import random
 import sys
+import json
 import pandas as pd
 from datetime import date
 from github import Github
@@ -11,97 +12,136 @@ try:
     import os
     from dotenv import load_dotenv
     load_dotenv()
-    GITHUB_API_KEY = os.environ['GITHUB_API_KEY']
-    print('Loaded API key from .env file.')
 except:
-    print('Could not load the .env file. Loading API key from environmental variable.')
-    GITHUB_API_KEY = os.environ['GITHUB_API_KEY']
+    print('Could not load the .env file. Will try to load API key from environmental variable.')
 
-df = pd.DataFrame(columns=['Listing ID','Price','Latitude','Longitude','District'])
+try:
+    GITHUB_API_KEY = os.environ['GITHUB_API_KEY']
+    g = Github(GITHUB_API_KEY)
+    print("Connected to GitHub via API key sucesfully.")
+except:
+    sys.exit("Cannot connect to GitHub - Arborting....")
+
+
+HEADERS = {
+    'Connection': 'keep-alive',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document',
+    'Accept-Language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5,de;q=0.4,cs;q=0.3,ja;q=0.2'
+    }
+
+URL = "https://www.bezrealitky.com/listings/offer-rent/flat/prague?page={page_no}"
+
+class Page:
+    def __init__(self,url):
+        time.sleep(random.randrange(1,4))
+        self.url = url
+        self.response = requests.get(self.url,headers=HEADERS)
+        self.soup = bs4.BeautifulSoup(self.response.text,"lxml")
+    
+    def get_html(self):
+        return str(self.soup)
+
+class ListingPage(Page):
+    def get_max_pages(self):
+        self.max_pages = int(self.soup.find_all("a",class_='page-link')[-2].text)
+        return self.max_pages
+
+    def get_listing_urls(self):
+        self.listings_in_page = self.soup.find_all("article",class_='PropertyCard_propertyCard__qPQRK propertyCard PropertyCard_propertyCard--landscape__7grmL')
+        self.apartment_urls = [listing.find("a",href=True).get("href") for listing in self.listings_in_page]
+        return self.apartment_urls     
+
+class Apartment(Page):
+    def get_parameters(self):
+        self.price_table = self.soup.find('table',{"class": "PriceTable_priceTable__voQsR priceTable"})
+        self.price_parameters = [instance.find("th").text.replace('Basic rent','Price').replace('Utility fees','Fees') for instance in self.price_table.find_all("tr")]
+        self.price_values = [instance.find("strong").text.replace('\xa0',' ') for instance in self.price_table.find_all("tr")]
+
+        self.characteristics_table = self.soup.find('section',{"class" : "box Section_section___TusU section mb-5 mb-lg-10"})
+        self.characteristics_parameters = [instance.find("th").text for instance in self.characteristics_table.find_all("tr")]
+        self.characteristics_values = [instance.find("td").text for instance in self.characteristics_table.find_all("tr")]
+
+        self.additional_information = self.soup.find_all("div",{"class" : "ParamsTable_paramsTable__Eqwx_ paramsTable"})[1]
+        self.additional_parameters = [instance.text for instance in self.additional_information.find_all("td") if instance.text.find('MHD') == -1 and instance.text.find('Front garden') == -1]
+
+        self.gps_json = json.loads(self.soup.find("script",{"id" : "__NEXT_DATA__"}).contents[0])
+
+        self.poi_json = json.loads(self.gps_json["props"]["pageProps"]["advert"]["poiData"])
+        
+        if self.poi_json is not None:
+            self.poi_names = [key for key in self.poi_json]
+            self.poi_values = [int(self.poi_json[key]['properties']['walkDistance']) for key in self.poi_names]
+        else:  
+            self.poi_names = []
+            self.poi_values = []
+
+        self.p = self.price_parameters + self.characteristics_parameters + self.additional_parameters + self.poi_names
+        self.v = self.price_values + self.characteristics_values + [1]*len(self.additional_parameters) + self.poi_values
+
+        self.dictionary_data = dict(zip(self.p,self.v))
+
+        self.dictionary_data['District'] = self.soup.find("span",{"class" : "PropertyAttributes_propertyAttributesItem__kscom"}).text
+
+        self.dictionary_data['Latitude'] = self.gps_json["props"]["pageProps"]["advert"]["gps"]["lat"]
+        self.dictionary_data['Longitude'] = self.gps_json["props"]["pageProps"]["advert"]["gps"]["lng"]
+
+        return self.dictionary_data
 
 current_page = 1
 max_pages = 999
 wait_time = 0
+df = pd.DataFrame()
 
 while current_page <= max_pages:
     while True:
-        try:  
-            time.sleep(random.randrange(1,4))
-            url = f'https://www.bezrealitky.com/listings/offer-rent/flat/praha?&page={current_page}'
-            headers =   {
-                        'Connection': 'keep-alive',
-                        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-                        'sec-ch-ua-mobile': '?0',
-                        'sec-ch-ua-platform': '"Windows"',
-                        'Upgrade-Insecure-Requests': '1',
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                        'Sec-Fetch-Site': 'none',
-                        'Sec-Fetch-Mode': 'navigate',
-                        'Sec-Fetch-User': '?1',
-                        'Sec-Fetch-Dest': 'document',
-                        'Accept-Language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5,de;q=0.4,cs;q=0.3,ja;q=0.2'
-                        }
-            response = requests.get(url,headers=headers)
-            soup = bs4.BeautifulSoup(response.text,"lxml")
-
-            max_pages = int(soup.find_all("a",class_='page-link pagination__page')[-2].text)
-            listings_in_page = soup.find_all(class_='product__link')
+        try:
+            listing_page_url = URL.format(page_no=current_page)
+            listing_page_ = ListingPage(url=listing_page_url)
+            apartment_url_list = listing_page_.get_listing_urls()
+            max_pages = listing_page_.get_max_pages()
             print(f'Parsing page {current_page} of {max_pages}')
         except:
-            wait_time = wait_time+120
-            print(f'Error during parsing page {current_page}. Waiting {wait_time/60} minutes and will try again')
-            if wait_time >= 1800:
+            wait_time = wait_time+300
+            print(f'Error during parsing the listing page: {listing_page_url} at {current_page} page. Waiting {int(wait_time/60)} minutes and will try again')
+            if wait_time >= 1500:
+                # err2 = listing_page_.get_html()
                 sys.exit("Too many trials. Arborting the script.")
             else:
                 time.sleep(wait_time)
             continue
         break
 
-    for apartment in listings_in_page:
+
+    for apartment_url in apartment_url_list:
         while True:
             try:
-                time.sleep(random.randrange(1,4))
-                apartment_url = apartment.get('href')
-                url2 = 'https://www.bezrealitky.com' + apartment_url
-                response2 = requests.get(url2,headers=headers)
-                soup2 = bs4.BeautifulSoup(response2.text,"lxml")
-                
-                parameters = soup2.find_all('div',{"class": "col col-6 param-title"})
-                values = soup2.find_all('div',{"class": "col col-6 param-value"})
-                parameters = [item.text.strip() for item in parameters]
-                values = [item.text.strip() for item in values]
-                
-                parameters2 = soup2.find_all('div',{"class": "col poi-item__name"})
-                values2 = soup2.find_all('div',{"class": "poi-item__walking"})
-                parameters2 = [item.text.strip()[:-1] for item in parameters2]
-                values2 = [item.find('strong').text.strip()[:-2] for item in values2]
-                
-                parameters = parameters + parameters2
-                values = values + values2
-
-                dictionary_data = dict(zip(parameters,values))
-                
-                dictionary_data['Latitude'] = soup2.find_all("div", {"class": "b-map__inner"})[0]['data-lat']
-                dictionary_data['Longitude'] = soup2.find_all("div", {"class": "b-map__inner"})[0]['data-lng']
-                dictionary_data['Listing ID'] = soup2.find('div',{'class':'col text-right align-self-center d-none d-lg-block'}).text.strip().replace('Listing ID: ','')
-                dictionary_data['District'] = soup2.find('div',{"class": "col col-12 col-md-8"}).find("h2").text.strip()
-
-                df_single_listing = pd.DataFrame([dictionary_data])
-                df = pd.concat([df,df_single_listing],axis=0,ignore_index=True)
+                apartment_ = Apartment(url=apartment_url)
+                apartment_parameters = apartment_.get_parameters()
+                df_ = pd.DataFrame([apartment_parameters])
+                df = pd.concat([df,df_],axis=0,ignore_index=True)
             except:
-                wait_time = wait_time+120
-                print(f'Error during parsing the url: {url2} at {current_page} page. Waiting {wait_time/60} minutes and will try again')
-                if wait_time >= 1800:
+                wait_time = wait_time+300
+                print(f'Error during parsing the apartment: {apartment_url} at {current_page} page. Waiting {int(wait_time/60)} minutes and will try again')
+                if wait_time >= 1500:
+                    # err1 = apartment_.get_html()
                     sys.exit("Too many trials. Arborting the script.")
                 else:
                     time.sleep(wait_time)
                 continue
             break
-    current_page += 1 
+
+    current_page = current_page + 1
 
 
-g = Github(GITHUB_API_KEY)
 repository = g.get_user().get_repo('Estate-rental-market-in-Prague')
 filename = date.today().strftime("data-rental/Prague-%Y-%m-%d.csv")
 content = df.to_csv(encoding='utf-8',index=False)
